51c47e790146cfd6b9adc91a1cdc82144c454ad4
AnthonyLopez-Github <126692944+AnthonyLopez-Github@users.noreply.github.com>
2023-06-07 02:52:17 -0400

    Fixed a lot of stuff. Added more functionality

--- PIC10F200_Nano_Programmer_Revised.ino
+++ PIC10F200_Nano_Programmer_Revised.ino
@@ -19,29 +19,29 @@ translate the address to the proper physical location when
 programming.
 
 ToDo:
-- Fix issue with setting configuration bits (Incorrect bits getting set)
+- Fix writeProgram function
 */
 
-#define MEMSIZE 384  // 1 word is 12 bits and the flash can store 256 words. [384 Bytes]
+#define MEMSIZE 256 // 1 word is 12 bits and the flash can store 256 words. [384 Bytes]
 #define WORDSIZE 12
-#define DEFAULT_CONFIG_WORD 0x7F18
-#define MCLRE (1 << 7)
-#define CP_NOT (1 << 6)
-#define WDTE (1 << 5)
+#define DEFAULT_CONFIG_WORD 0x0FE3
+#define MCLRE (1 << 4)
+#define CP_NOT (1 << 3)
+#define WDTE (1 << 2)
 
 const unsigned int ICSPCLK = 2;
 const unsigned int ICSPDAT = 4;
 
-// Least significant bit first
-const byte LOAD_COMMAND = 0b01000000;        // Load Data for Program Memory
-const byte READ_COMMAND = 0b00100000;        // Read Data from Program Memory
-const byte INCREMENT_COMMAND = 0b01100000;   // Increment Address
-const byte BEGIN_COMMAND = 0b00010000;       // Begin Programming
-const byte END_COMMAND = 0b01110000;         // End Programming
-const byte BULK_ERASE_COMMAND = 0b10010000;  // Bulk Erase Program Memory
+// Most significant bit first
+const byte LOAD_COMMAND = 0b0010;        // Load Data for Program Memory
+const byte READ_COMMAND = 0b0100;        // Read Data from Program Memory
+const byte INCREMENT_COMMAND = 0b0110;   // Increment Address
+const byte BEGIN_COMMAND = 0b1000;       // Begin Programming
+const byte END_COMMAND = 0b1110;         // End Programming
+const byte BULK_ERASE_COMMAND = 0b1001;  // Bulk Erase Program Memory
 
 byte program[MEMSIZE];  // Array containing 384 bytes
-const uint16_t configurationWord = DEFAULT_CONFIG_WORD | CP_NOT; // Set to: 0111111101011000. Meaning: GP3/MCLR functions as GP3, Code protection is off, Watchdog timer is disabled.
+const uint16_t configurationWord = DEFAULT_CONFIG_WORD | CP_NOT | WDTE; // Set to: 111111101011. Meaning: GP3/MCLR functions as GP3, Code protection is off, Watchdog timer is disabled.
 uint16_t osccalPrimaryCalibrationBits; // Storage for calibration bits in program memory
 uint16_t osccalBackupCalibrationBits; // Storage for calibration bits in configuration memory
 
@@ -49,52 +49,75 @@ void incrementAddress(void) {
   for(int i = 0 ; i < 6; i++) {
     digitalWrite(ICSPCLK, HIGH);
     delay(1);
-    digitalWrite(ICSPDAT, ((INCREMENT_COMMAND >> (2 + (5 - i))) & 0b00000001) ? HIGH : LOW);
+    digitalWrite(ICSPDAT, ((INCREMENT_COMMAND >> i) & 0b00000001) ? HIGH : LOW);
     delay(1);
     digitalWrite(ICSPCLK, LOW);
     delay(1);
   }
 }
 
-// NOTE: word will need to be formatted: 0xxxxxxxxxxxx0000, where the x's are the word. Lsb first.
+// Load a word
 void loadWord(uint16_t word) {
   // Send write command
   for(int i = 0 ; i < 6; i++) {
     digitalWrite(ICSPCLK, HIGH);
     delay(1);
-    digitalWrite(ICSPDAT, ((LOAD_COMMAND >> (2 + (5 - i))) & 0b00000001) ? HIGH : LOW);
+    digitalWrite(ICSPDAT, ((LOAD_COMMAND >> i) & 0b00000001) ? HIGH : LOW);
     delay(1);
     digitalWrite(ICSPCLK, LOW);
     delay(1);
   }
 
-  // LSb first
-  for(int i = 0 ; i < 16 ; i++) {
+  // Skip first bit
+  digitalWrite(ICSPDAT, LOW);
+  digitalWrite(ICSPCLK, HIGH);
+  delay(1);
+  digitalWrite(ICSPCLK, LOW);
+  delay(1);
+
+  // Load word
+  for(int i = 0 ; i < 12 ; i++) {
     digitalWrite(ICSPCLK, HIGH);
     delay(1);
-    digitalWrite(ICSPDAT, ((word >> (15 - i)) & 0b00000001) ? HIGH : LOW);
+    digitalWrite(ICSPDAT, ((word >> i) & (uint16_t) 0x1) ? HIGH : LOW);
     delay(1);
     digitalWrite(ICSPCLK, LOW);
     delay(1);
   }
+
+  // Skip the remaining bits
+  digitalWrite(ICSPDAT, LOW);
+  digitalWrite(ICSPCLK, HIGH);
+  delay(1);
+  digitalWrite(ICSPCLK, LOW);
+  delay(1);
+  digitalWrite(ICSPCLK, HIGH);
+  delay(1);
+  digitalWrite(ICSPCLK, LOW);
+  delay(1);
+  digitalWrite(ICSPCLK, HIGH);
+  delay(1);
+  digitalWrite(ICSPCLK, LOW);
+  delay(1);
 }
 
 void beginProgramming(void) {
   for(int i = 0 ; i < 6; i++) {
     digitalWrite(ICSPCLK, HIGH);
     delay(1);
-    digitalWrite(ICSPDAT, ((BEGIN_COMMAND >> (2 + (5 - i))) & 0b00000001) ? HIGH : LOW);
+    digitalWrite(ICSPDAT, ((BEGIN_COMMAND >> i) & 0b00000001) ? HIGH : LOW);
     delay(1);
     digitalWrite(ICSPCLK, LOW);
-    delay(3); // Requires a max of 2ms wait time before using end programming
+    delay(1);
   }
+  delay(2); // Wait at most 2ms before using end programming
 }
 
 void endProgramming(void) {
   for(int i = 0 ; i < 6; i++) {
     digitalWrite(ICSPCLK, HIGH);
     delay(1);
-    digitalWrite(ICSPDAT, ((END_COMMAND >> (2 + (5 - i))) & 0b00000001) ? HIGH : LOW);
+    digitalWrite(ICSPDAT, ((END_COMMAND >> i) & 0b00000001) ? HIGH : LOW);
     delay(1);
     digitalWrite(ICSPCLK, LOW);
     delay(1);
@@ -108,7 +131,7 @@ uint16_t readWord(void) {
   for(int i = 0 ; i < 6; i++) {
     digitalWrite(ICSPCLK, HIGH);
     delay(1);
-    digitalWrite(ICSPDAT, ((READ_COMMAND >> (2 + (5 - i))) & 0b00000001) ? HIGH : LOW);
+    digitalWrite(ICSPDAT, ((READ_COMMAND >> i) & 0b00000001) ? HIGH : LOW);
     delay(1);
     digitalWrite(ICSPCLK, LOW);
     delay(1);
@@ -154,24 +177,101 @@ uint16_t readWord(void) {
 
   return wordBuffer;
 }
+////////////////////////////////////////////////////////////////////////////////////////////////
+int getBit(byte * data, int queryBit) {
+  int byteIndex = queryBit / 8;
+  int bitIndex = queryBit % 8;
 
-void getCalibrationBits(void) {
+  return data[byteIndex] & (0x1 << (7 - bitIndex));
+}
+////////////////////////////////////////////////////////////////////////////////////////////////
+void storeCalibrationBits(void) {
   // Move to OSCCAL calibration bits in program memory
   for(int i = 0 ; i < 256 ; i++) {
     incrementAddress();
   }
 
-  osccalPrimaryCalibrationBits = readWord();
-  Serial.print(osccalPrimaryCalibrationBits, BIN);
-  Serial.println();
+  osccalPrimaryCalibrationBits = readWord(); // Store
 
   // Move to OSCCAL backup calibration bits in configuration memory
   for(int i = 0 ; i < 5 ; i++){
     incrementAddress();
   }
 
-  osccalBackupCalibrationBits = readWord();
-  Serial.print(osccalBackupCalibrationBits, BIN);
+  osccalBackupCalibrationBits = readWord(); // Store
+}
+
+void writeCalibrationBits(void) {
+  // Move to OSCCAL calibration bits in program memory
+  for(int i = 0 ; i < 256 ; i++) {
+    incrementAddress();
+  }
+
+  // Store primary calibration bits at current address
+  loadWord(osccalPrimaryCalibrationBits);
+  beginProgramming();
+  endProgramming();
+
+  // Move to OSCCAL backup calibration bits in configuration memory
+  for(int i = 0 ; i < 5 ; i++){
+    incrementAddress();
+  }
+
+  // Store backup calibration bits at current address
+  loadWord(osccalBackupCalibrationBits);
+  beginProgramming();
+  endProgramming();
+}
+
+void writeConfigurationWord(void) {
+  Serial.print("Writing configuration word...");
+  loadWord(configurationWord);
+  beginProgramming();
+  endProgramming();
+
+  // Report if written value does not match what was provided
+  uint16_t valueWritten = readWord();
+  if(valueWritten != configurationWord) {
+    Serial.println("Failed to write configuration word: ");
+    Serial.print("Provided: ");
+    Serial.println(configurationWord, BIN);
+    Serial.print("Read: ");
+    Serial.println(valueWritten, BIN);
+  } else {
+    Serial.println("Done");
+    Serial.print("Current configuration: ");
+    Serial.println(valueWritten, BIN);
+  }
+}
+
+int writeProgram(byte programArr[MEMSIZE]) {
+  incrementAddress(); // Roll over to address 000 from configuration word
+
+  uint16_t wordBuffer;
+  int bitIndex = 0;
+  Serial.print("Writing program to memory...");
+  for(int i = 0 ; i < MEMSIZE - 1 ; i++) {
+    wordBuffer = 0; // Reset buffer
+    for(int j = 0 ; j < 12 ; j++) {
+      wordBuffer |= getBit(programArr, bitIndex) << (11 - j);
+      bitIndex ++;
+    }
+    loadWord(wordBuffer);
+    beginProgramming();
+    endProgramming();
+
+    uint16_t valueWritten = readWord();
+    if(valueWritten != wordBuffer) {
+      Serial.println("Failed to write word: ");
+      // Add address with error
+      Serial.print("Provided: ");
+      Serial.println(wordBuffer, BIN);
+      Serial.print("Read: ");
+      Serial.println(valueWritten, BIN);
+      return 1;
+    }
+  }
+  Serial.println("Done");
 }
 
 void setup() {
@@ -192,15 +292,23 @@ void setup() {
   while(!Serial.available()) {
   }
 
-  loadWord(configurationWord);
-  beginProgramming();
-  endProgramming();
+  writeConfigurationWord();
 
-  Serial.print(readWord(), BIN);
-  //getCalibrationBits();
+  /*
+  // Write a bunch of NOPs
+  writeProgram(program); 
+  
+  // Increment twice to rollover to 000
+  incrementAddress();
+  incrementAddress();
+
+  // Dump memory
+  for(int i = 0 ; i < 256 ; i++) {
+    readWord();
+    incrementAddress();
+  }
+  */
 }
 
 void loop() {
-  //digitalWrite(ICSPDAT, LOW);
-  //digitalWrite(ICSPCLK, LOW);
 }
